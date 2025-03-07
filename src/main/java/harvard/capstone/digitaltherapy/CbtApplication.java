156c5577d09f8b45e12d2430f2ce95f783fd1ef5
gumbergu <gundeep.gumber@adp.com>
2025-03-06 22:18:25 -0500

    [KAN-4]:Setup Backend Java Springboot environment to authenticate and store login.

---
+++ src/main/java/harvard/capstone/digitaltherapy/CbtApplication.java
@@ -0,0 +1,13 @@
+package harvard.capstone.digitaltherapy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CbtApplication.class, args);
+	}
+
+}
