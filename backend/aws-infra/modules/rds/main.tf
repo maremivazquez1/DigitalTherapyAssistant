8214d5e553f47fd78e66c103cf40664b157ab23d
Marco Vazquez <maremivazquez@hotmail.com>
2025-03-22 00:19:00 +0000

    updated rds terraform config

--- backend/aws-infra/modules/rds/main.tf
+++ backend/aws-infra/modules/rds/main.tf
@@ -1,12 +1,17 @@
+data "aws_db_instance" "existing" {
+  db_instance_identifier = "my-dta-db"
+}
+
 resource "aws_db_instance" "rds_instance" {
-  identifier          = "my-dta-db"
-  engine              = "mysql"
-  instance_class      = "db.t4g.micro"
-  allocated_storage   = 20
-  max_allocated_storage = 100  # Max storage to scale up to (in GB)
-  db_name             = var.db_name
+  count = length(data.aws_db_instance.existing) > 0 ? 0 : 1
+
+  identifier            = "my-dta-db"
+  allocated_storage     = 20
+  storage_type          = "gp2"
+  engine               = "mysql"
+  engine_version       = "8.0"
+  instance_class       = "db.t3.micro"
   username            = "admin"
-  password            = "securepassword"
+  password            = "yourpassword"
   skip_final_snapshot = true
-  backup_retention_period = 1
 }
