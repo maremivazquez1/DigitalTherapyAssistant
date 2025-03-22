99a909ec082e4dab3b3b7745d89dcc762f93202d
Marco Vazquez <maremivazquez@hotmail.com>
2025-03-22 14:39:42 +0000

    added key check

--- backend/aws-infra/main.tf
+++ backend/aws-infra/main.tf
@@ -23,16 +23,23 @@ data "aws_ami" "latest_amazon_linux" {
   }
 }
 
+# check if key already exists and let count be 0 
+# in the next sections to skip creating another
+data "aws_key_pair" "existing" {
+  key_name = "var.key_name"
+}
 
 # private key is used to ssh (will be output to console)
 # Remove output when no longer need to ssh
 resource "tls_private_key" "key" {
+  count   = length(data.aws_key_pair.existing) > 0 ? 0 : 1
   algorithm = "RSA"
   rsa_bits  = 2048
 }
 
 # generate public key to authenticate to ec2
 resource "aws_key_pair" "my_ssh_key" {
+  count   = length(data.aws_key_pair.existing) > 0 ? 0 : 1
   key_name   = var.key_name
   public_key = tls_private_key.key.public_key_openssh
   depends_on = [tls_private_key.key]
