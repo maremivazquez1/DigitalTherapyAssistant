56ae968ccc5736959a80d9f8895901adfc5402d2
shelbyGrasso <shelbyagrasso@gmail.com>
2025-03-24 23:09:38 -0400

    made cbt session the homepage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,14 +4,13 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import Dashboard from "./pages/Dashboard";
 import CBT from "./pages/CBT";
 
 const App: React.FC = () => {
   return (
     <div data-theme="calming">
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        <Route path="/" element={<CBT />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/cbt" element={<CBT />} />
