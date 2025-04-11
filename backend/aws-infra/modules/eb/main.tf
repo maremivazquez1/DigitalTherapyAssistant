9ac755fcbd1d96eaab877387bd6bfa98eaf581e2
Marco Vazquez <maremivazquez@hotmail.com>
2025-04-11 21:11:11 +0000

    fixed warning

--- backend/aws-infra/modules/eb/main.tf
+++ backend/aws-infra/modules/eb/main.tf
@@ -5,6 +5,10 @@ resource "aws_elastic_beanstalk_application" "this" {
 
 resource "aws_s3_bucket" "artifact_bucket" {
   bucket = var.s3_bucket
+}
+
+resource "aws_s3_bucket_acl" "artifact_bucket_acl" {
+  bucket = aws_s3_bucket.artifact_bucket.id
   acl    = "private"
 }
 
