283a9f7070274f73fd1bbae4adabb64933a2ff51
Marco Vazquez <maremivazquez@hotmail.com>
2025-04-11 21:32:59 +0000

    updated bucket name

--- backend/aws-infra/main.tf
+++ backend/aws-infra/main.tf
@@ -53,10 +53,10 @@ module "eb" {
   application_name    = "springboot-app"
   environment_name    = "springboot-env"
   artifact_path       = "build/libs/springboot-app.zip" # Update with your actual ZIP file location
-  artifact_key        = "springboot-app.zip"
+  artifact_key        = var.s3_key
   solution_stack_name = "64bit Amazon Linux 2 v3.4.10 running Corretto 17"
   instance_type       = "t3.micro"
-  s3_bucket           = "beanstalk-artifacts"
+  s3_bucket           = "var.s3_bucket
 
   # from rds
   db_host     = module.rds.rds_endpoint
--- backend/aws-infra/variables.tf
+++ backend/aws-infra/variables.tf
@@ -4,3 +4,5 @@ variable "key_name" {}
 variable "db_name" {}
 variable "rds_exists" {}
 variable "vpc_id" { description = "overall vpc" }
+variable "s3_bucket" {}
+variable "s3_key" {}
